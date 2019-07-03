8e2f9a41ecf93cadf53d04aed3d5d6935e94744b
Nuttasak Tawon <nuttasak@sobtid.me>
2019-07-03 14:17:57 +0700

    cancel request

--- src/routes/group/index.js
+++ src/routes/group/index.js
@@ -9,6 +9,7 @@ const groupDelete = require('./delete')
 const listOfRequestor = require('./list-requestor')
 const studentRequestToJoinGroup = require('./request-join')
 const approveStudentToJoinGroup = require('./approve')
+const studentCancelRequestToJoinGroup = require('./cancel')
 
 
 module.exports = async (fastify, options) => {
@@ -21,6 +22,7 @@ module.exports = async (fastify, options) => {
 
   fastify.register(listOfRequestor)
   fastify.register(studentRequestToJoinGroup)
+  fastify.register(studentCancelRequestToJoinGroup)
   fastify.register(approveStudentToJoinGroup)
 
   // fastify.register(require('./student'))
