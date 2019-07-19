02fecb1ccca9d93cab0969d4e8443fcefd75b942
Nuttasak Tawon <nuttasak@sobtid.me>
2019-07-19 10:16:39 +0700

    Fix update

--- src/routes/group/update.js
+++ src/routes/group/update.js
@@ -14,19 +14,21 @@ module.exports = async (fastify, options) => {
     bodyLimit: 2200000 // limit 2.2 mb
   }, async (request, reply) => {
     
-    const { body, params } = request.params
-    
+    const { body, params } = request
+
     const group = await fastify.mongoose.Group.findOne({ _id: params.groupId })
     if (!group) throw  fastify.httpErrors.notFound(fastify.message('group.notFound'))
+    
+    group.name = body.name
 
-    if (body.logo && body.logo.include('data:image/')) {
+    if (body.logo && body.logo.includes('data:image/')) {
       const filename = `group-${group._id}`
       const imageInfo = fastify.storage.diskGroupLogo(body.logo, filename)
       
-      body.logo = imageInfo.fileName
+      group.logo = imageInfo.fileName
     }
-    
-    const result = await fastify.mongoose.Group.findOneAndUpdate({ _id: params.groupId }, body)
+
+    const result = await fastify.mongoose.Group.findOneAndUpdate({ _id: params.groupId }, group)
     
     return { message: fastify.message('group.updated') }
   })
