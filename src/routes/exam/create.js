65834be1abc116a859221de1e4b3f889cc9f4041
nutsobtid <nuttasak@sobtid.me>
2019-08-01 12:27:50 +0700

    WIP

--- src/routes/exam/create.js
+++ src/routes/exam/create.js
@@ -1,6 +1,7 @@
 'use strict' 
 
 const { ROLE } = require('@config/user')
+const { CRITERION, EXAM_TYPE } = require('@config/exam')
 const moemnt = require('moment')
 
 module.exports = async (fastify) => { 
@@ -20,22 +21,44 @@ module.exports = async (fastify) => {
 }
 
 const mapExamParams = (user, params) => {
-  if (params.itemType == 'G') return generalExamType(user, params)
+  if (params.itemType == EXAM_TYPE.GENERAL) return generalExamType(user, params)
 }
 
 const generalExamType = (user, params) => {
-  const RequestedName = composeRequestName(user)
-  const RequestType = getRequestType(user)
-  const RequestedNo = `${RequestedName}FixedRandomTestset${RequestType}${moemnt.defaultFormat('YYYYMMDDHHmmSSS')}`
-  const TestSetType = params.quantity > 1 ? 'RI' : 'FI'
-  return {
-    RequestedName,
-    RequestType,
-    RequestedNo,
-    TestSetType
+  const params = {
+    RequestedName: composeRequestName(user),
+    RequestType: getRequestType(user),
+    RequestedNo: `${composeRequestName(user)}FixedRandomTestset${getRequestType(user)}${moemnt.defaultFormat('YYYYMMDDHHmmSSS')}`,
+    TestSetType: getTestSetType(params.quantity),
+    ItemType: EXAM_TYPE.GENERAL,
+    KeyStage: params.learningArea,
+    NoItems: params.quantity,
+    ComplexityLevel: params.level
   }
+
+  Object.assign(params, mapCriterion(params))
+
+  return params
 }
 
 const composeRequestName = (user) => `${ures.firstName} ${user.lastName}`
 
 const getRequestType = (user) => user.role == ROLE.STUDENT ? 2 : 1
+
+const getTestSetType = (quantity) => quantity > 1 ? 'RI' : 'FI'
+
+const mapCriterion = (params) => {
+  switch (params.criterion) {
+    case CRITERION.INDICATOR:
+      return {
+        FollowIndicator: true,
+        Indicator: params.indicators.map(indicator => `${indicator.code},${indicator.questionType},${indicator.quantity}`).join(';')
+      }
+    case CRITERION.STRAND:
+      return {
+        FollowStrand: true,
+        Strand: params.strands.map(strand => `${strand.code},${strand.questionType},${strand.quantity}`).join(';')
+      }
+    default:
+  }
+}
