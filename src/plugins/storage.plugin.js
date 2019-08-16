69009156eec3961c304febfad1bf6da994b8af47
nutsobtid <nuttasak@sobtid.me>
2019-08-16 09:43:48 +0700

    Fix storage

--- src/plugins/storage.plugin.js
+++ src/plugins/storage.plugin.js
@@ -1,23 +1,24 @@
 const fp = require('fastify-plugin')
 const base64ToImage = require('base64-to-image')
 const path = require('path')
+const { PROFILE_IMAGE_PATH, GROUP_LOGO_PATH } = require('@config/storage')
 
 module.exports = fp( async (fastify, options) => {
   fastify.decorate('storage', {
     diskProfileImage: (dataBase64, fileName, imageType = 'png') => {
-      const dirpath = path.resolve(fastify.config.PROFILE_IMAGE_PATH)
+      const dirpath = path.resolve(PROFILE_IMAGE_PATH)
       const option = { fileName, imageType }
       return base64ToImage(dataBase64, `${dirpath}/`, option)
     },
     
-    getUrlProfileImage: (filename) =>  `${fastify.env.APP_URL}/${fastify.config.PROFILE_IMAGE_PATH}/${ filename ? filename : 'default.png'}`, // return default image if null or empty
+    getUrlProfileImage: (filename) =>  `${fastify.env.APP_URL}/${PROFILE_IMAGE_PATH}/${ filename ? filename : 'default.png'}`, // return default image if null or empty
     
     diskGroupLogo: (dataBase64, fileName, imageType = 'png') => {
-      const dirpath = path.resolve(fastify.config.GROUP_LOGO_PATH)
+      const dirpath = path.resolve(GROUP_LOGO_PATH)
       const option = { fileName, imageType }
       return base64ToImage(dataBase64, `${dirpath}/`, option)
     },
     
-    getUrlGroupLogo: (filename) => `${fastify.env.APP_URL}/${fastify.config.GROUP_LOGO_PATH}/${filename ? filename : 'default.png'}` 
+    getUrlGroupLogo: (filename) => `${fastify.env.APP_URL}/${GROUP_LOGO_PATH}/${filename ? filename : 'default.png'}` 
   })
 })
