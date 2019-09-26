6708992c676d34c4d92540503a96e997874b2b82
nutsobtid <nuttasak@sobtid.me>
2019-09-26 16:42:32 +0700

    Fixed student leave group

--- src/routes/group/student/leave.js
+++ src/routes/group/student/leave.js
@@ -18,22 +18,14 @@ module.exports = async function(fastify, opts, next) {
     const group = await fastify.mongoose.Group.findOne({ _id: params.groupId }).select('students')
     if (!group) throw  fastify.httpErrors.notFound(fastify.message('group.notFound'))
 
-    const { inGroup } = group.students
-
-    if (inGroup.findIndex(student => student.userInfo.toString() === user._id.toString()) === -1) {
-      throw fastify.httpErrors.badRequest(fastify.message('group.did_not_in_group'))
-    }
-
-    const student = { userInfo: user._id }
-
     await Promise.all([
       fastify.mongoose.Group.updateOne({
         _id: group._id,
-        students: { $elemMatch: { userInfo: { $in: studentIds } } }
+        students: { $elemMatch: { userInfo: user._id } }
       }, { 
         $set: { 
-          'students.$[elem].status': STUDENT_STATUS.LEFT, 
-          'students.$[elem].leftDate': moment().unix() 
+          'students.$.status': STUDENT_STATUS.LEFT, 
+          'students.$.leftDate': moment().unix() 
         } 
       }),
       fastify.mongoose.User.updateOne({_id: user._id}, { $pull: { groups: { info: group._id } } })
