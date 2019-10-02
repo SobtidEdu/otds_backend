65be4a0521347ba9bfd1a8df37ea63cea4c5923f
nutsobtid <nuttasak@sobtid.me>
2019-10-02 14:49:37 +0700

    Fix name params

--- src/routes/report/group.js
+++ src/routes/report/group.js
@@ -11,37 +11,16 @@ module.exports = async (fastify, options) => {
       fastify.authorize([ROLE.TEACHER, ROLE.SUPER_TEACHER, ROLE.ADMIN])
     ]
   }, async (request) => {
-    // const { user, params } = request
-
-    // const aggregate = [
-    //   {
-    //     $match: { 
-    //       $and: {
-    //         userId: user._id, 
-    //         "exams._id": params.examId
-    //       }
-    //     }
-    //   },
-    //   {
-    //     $project: {
-    //       logo: 1,
-    //       name: 1,
-    //       studentCount: 1
-    //     }
-    //   }
-    // ]
-    // const groupsReport = await fastify.mongoose.Group.aggregate(aggregate)
-    // return groupsReport
     return [{
       _id: 1,
       logo: null,
       name: 'ป.1/1',
       totalStudent: 30,
       totalStudentTest: 4,
-      startedAt: 1569572003,
+      latestStartedAt: 1569572003,
       minScore: 10,
       maxScore: 20,
-      minScore: 15,
+      avgScore: 15,
     }]
   })
 
@@ -55,25 +34,25 @@ module.exports = async (fastify, options) => {
       logo: null,
       name: 'ด.ช. ราตรี สูงนาน',
       schoolName: 'มัธยมด่านสำโรง',
-      startedAt: 1569572003,
+      latestStartedAt: 1569572003,
       totalTest: 3,
-      score: 20
+      latestScore: 20
     },
     {
       logo: null,
       name: 'ด.ช. สมศักดิ์ ชูจิต',
       schoolName: 'มัธยมวัดสิงห์',
-      startedAt: 1569572003,
+      latestStartedAt: 1569572003,
       totalTest: 3,
-      score: 18
+      latestScore: 18
     },
     {
       logo: null,
       name: 'ด.ช. mother fucker',
       schoolName: 'มัธยมวัดสิงห์',
-      startedAt: 1569572003,
+      latestStartedAt: 1569572003,
       totalTest: 4,
-      score: 17
+      latestScore: 17
     }]
   })
 }
