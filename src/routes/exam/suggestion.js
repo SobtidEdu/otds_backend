c68b45cae73bb87880e7a2947b5d2f4df7fcb496
nutsobtid <nuttasak@sobtid.me>
2019-10-22 18:42:22 +0700

    remove suggestion

--- src/routes/exam/suggestion.js
+++ src/routes/exam/suggestion.js
@@ -26,4 +26,20 @@ module.exports = async (fastify, opts) => {
 
     return { message: 'Has been add suggestion' }
   })
+
+  fastify.delete('/:examId/suggestion', {
+    preValidation: [
+      fastify.authenticate(),
+      fastify.authorize([ ROLE.ADMIN ])
+    ]
+  }, async (request) => {
+    const { params } = request
+    
+    const exam = await fastify.mongoose.Exam.findOne({ _id: params.examId })
+    if (!exam) throw fastify.httpErrors.badRequest('Not found exam id')
+
+    await fastify.mongoose.ExamSuggestion.updateOne({}, { $pull: {  list: { exam: exam._id } } })
+
+    return { message: 'Has been remove suggestion' }
+  })
 }
