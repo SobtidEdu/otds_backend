389bc7a1eee8f4245443a2cf957131fe37addabb
nutsobtid <nuttasak@sobtid.me>
2019-11-06 10:55:34 +0700

    Fixed group detail

--- src/routes/exam/detail.js
+++ src/routes/exam/detail.js
@@ -15,7 +15,7 @@ module.exports = async (fastify) => {
     ]
   }, async (request) => {
     
-    const { params, query } = request
+    const { params, query, user } = request
 
     try {
       let exam = {}
@@ -31,10 +31,17 @@ module.exports = async (fastify) => {
           throw fastify.httpErrors.notFound(`ไม่พบชุดข้อสอบ`)
         }
         exam.owner = await fastify.mongoose.User.findOne({ _id: exam.owner }).select('_id firstName lastName prefixName, role')
+
         exam.isContinueTesting = await fastify.mongoose.Testing.count({ examId: exam._id, finishedAt: null })
       }
 
-      return exam
+      const queryTesting = { examId: exam._id, userId: user._id, groupId: { $exists: false }, }
+      if (query.groupId) {
+        queryTesting.groupId = query.groupId
+      }
+      const testing = await fastify.mongoose.Testing.find(queryTesting).sort({ createdAt: -1 }).limit(1)
+
+      return { ...exam, testing }
     } catch (e) {
       console.log(e)
       if (e.kind === 'ObjectId') throw fastify.httpErrors.notFound(`Not found exam id ${params.examId}`)
