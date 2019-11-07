85f138c19152c8fadd0d4d4a01193850861d4337
nutsobtid <nuttasak@sobtid.me>
2019-11-08 03:30:13 +0700

    Fixed statistic

--- src/routes/statistic/exam.js
+++ src/routes/statistic/exam.js
@@ -101,7 +101,7 @@ module.exports = async (fastify, options) => {
     }))
   })
 
-  fastify.get('/exam/detail/:year/:month/type', {
+  fastify.get('/exam/detail/:year/:month/:type', {
     preValidation: [
       fastify.authenticate(),
       fastify.authorize([ROLE.ADMIN])
@@ -112,29 +112,214 @@ module.exports = async (fastify, options) => {
     const start = moment(`${params.year}${params.month}01000000 `, "YYYYMMDDHHmmss").unix()
     const end = moment(`${params.year}${params.month}30235959 `, "YYYYMMDDHHmmss").unix()
 
-    const aggregate = [
-      {
-        $match: {
-          createdAt: {
-            $gte: start,
-            $lt: end
+    if (params.type == 'examType') {
+      const aggregate = [
+        {
+          $match: {
+            createdAt: {
+              $gte: start,
+              $lt: end
+            }
+          }
+        },
+        {
+          $group: {
+            _id: {
+              type: "$type",
+            },
+            count: { $sum: 1 }
           }
         }
-      },
-      {
-        $group: {
-          _id: {
-            type: "$type",
-          },
-          count: { $sum: 1 }
-        }
-      }
-    ] 
+      ] 
+  
+      const response = await fastify.mongoose.Exam.aggregate(aggregate)
+      return response.map((stat) => ({
+        type: stat._id.type,
+        total: stat.count
+      }))
+    }
 
-    const response = await fastify.mongoose.Exam.aggregate(aggregate)
-    return response.map((stat) => ({
-      type: stat._id.type,
-      total: stat.count
-    }))
+    if (params.type == 'grade') {
+      const aggregate = [
+        {
+          $match: {
+            createdAt: {
+              $gte: start,
+              $lt: end
+            }
+          }
+        },
+        {
+          $lookup: {
+            from: 'users',
+            localField: 'owner',
+            foreignField: '_id',
+            as: 'user'
+          }
+        },
+        {
+          $unwind: "$user"
+        },
+        {
+          $project: {
+            grade: 1,
+            student: {
+              $cond: [{ $eq: ["$user.role", ROLE.STUDENT] }, 1, 0]
+            },
+            teacher: {
+              $cond: [{ $eq: ["$user.role", ROLE.TEACHER] }, 1, 0]
+            },
+            superTeacher: {
+              $cond: [{ $eq: ["$user.role", ROLE.SUPER_TEACHER] }, 1, 0]
+            },
+            admin: {
+              $cond: [{ $eq: ["$user.role", ROLE.ADMIN] }, 1, 0]
+            }
+          }
+        },
+        {
+          $group: {
+            _id: {
+              grade: "$grade",
+            },
+            student: { $sum: "$student"},
+            teacher: { $sum: "$teacher"},
+            superTeacher: { $sum: "$superTeacher"},
+            admin: { $sum: "$admin"},
+          }
+        }
+      ]
+
+      const response = await fastify.mongoose.Exam.aggregate(aggregate)
+      return response.map((stat) => ({
+        grade: stat._id.grade,
+        student: stat.student,
+        teacher: stat.teacher,
+        superTeacher: stat.superTeacher,
+        admin: stat.admin
+      }))
+    }
+    
+    if (params.type == 'criterion') {
+      const aggregate = [
+        {
+          $match: {
+            createdAt: {
+              $gte: start,
+              $lt: end
+            }
+          }
+        },
+        {
+          $lookup: {
+            from: 'users',
+            localField: 'owner',
+            foreignField: '_id',
+            as: 'user'
+          }
+        },
+        {
+          $unwind: "$user"
+        },
+        {
+          $project: {
+            criterion: 1,
+            student: {
+              $cond: [{ $eq: ["$user.role", ROLE.STUDENT] }, 1, 0]
+            },
+            teacher: {
+              $cond: [{ $eq: ["$user.role", ROLE.TEACHER] }, 1, 0]
+            },
+            superTeacher: {
+              $cond: [{ $eq: ["$user.role", ROLE.SUPER_TEACHER] }, 1, 0]
+            },
+            admin: {
+              $cond: [{ $eq: ["$user.role", ROLE.ADMIN] }, 1, 0]
+            }
+          }
+        },
+        {
+          $group: {
+            _id: {
+              criterion: "$criterion",
+            },
+            student: { $sum: "$student"},
+            teacher: { $sum: "$teacher"},
+            superTeacher: { $sum: "$superTeacher"},
+            admin: { $sum: "$admin"},
+          }
+        }
+      ]
+
+      const response = await fastify.mongoose.Exam.aggregate(aggregate)
+      return response.map((stat) => ({
+        criterion: stat._id.criterion,
+        student: stat.student,
+        teacher: stat.teacher,
+        superTeacher: stat.superTeacher,
+        admin: stat.admin
+      }))
+    }
+
+    if (params.type == 'subject') {
+      const aggregate = [
+        {
+          $match: {
+            createdAt: {
+              $gte: start,
+              $lt: end
+            }
+          }
+        },
+        {
+          $lookup: {
+            from: 'users',
+            localField: 'owner',
+            foreignField: '_id',
+            as: 'user'
+          }
+        },
+        {
+          $unwind: "$user"
+        },
+        {
+          $project: {
+            subject: 1,
+            student: {
+              $cond: [{ $eq: ["$user.role", ROLE.STUDENT] }, 1, 0]
+            },
+            teacher: {
+              $cond: [{ $eq: ["$user.role", ROLE.TEACHER] }, 1, 0]
+            },
+            superTeacher: {
+              $cond: [{ $eq: ["$user.role", ROLE.SUPER_TEACHER] }, 1, 0]
+            },
+            admin: {
+              $cond: [{ $eq: ["$user.role", ROLE.ADMIN] }, 1, 0]
+            }
+          }
+        },
+        {
+          $group: {
+            _id: {
+              subject: "$subject",
+            },
+            student: { $sum: "$student"},
+            teacher: { $sum: "$teacher"},
+            superTeacher: { $sum: "$superTeacher"},
+            admin: { $sum: "$admin"},
+          }
+        }
+      ]
+
+      const response = await fastify.mongoose.Exam.aggregate(aggregate)
+      return response.map((stat) => ({
+        subject: stat._id.subject,
+        student: stat.student,
+        teacher: stat.teacher,
+        superTeacher: stat.superTeacher,
+        admin: stat.admin
+      }))
+    }
   })
 }
