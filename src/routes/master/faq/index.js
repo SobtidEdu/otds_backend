a42e2b6ab761fe5d243e0522fb7a2b71eef482fe
nutsobtid <nuttasak@sobtid.me>
2019-11-07 21:47:41 +0700

    Fixed faq

--- src/routes/master/faq/index.js
+++ src/routes/master/faq/index.js
@@ -5,5 +5,4 @@ module.exports = async (fastify, options) => {
   fastify.register(require('./create'))
   fastify.register(require('./update'))
   fastify.register(require('./remove'))
-  fastify.register(require('./contact'))
 }
