1a90ff940ba6024547c9ebd5b9a0eb5e2fdbd887
nutsobtid <nuttasak@sobtid.me>
2019-11-18 12:09:02 +0700

    Fixed delete subject

--- src/routes/master/subject/delete.js
+++ src/routes/master/subject/delete.js
@@ -14,7 +14,7 @@ module.exports = async (fastify, opts) => {
   }, async (request) => {
     const { params } = request
 
-    return await fastify.mongoose.ExamConfiguration.update({ type: 'SUBJECT' }, { $pull: { data: { id: params.id } } })
+    return await fastify.mongoose.ExamConfiguration.update({ type: 'SUBJECT' }, { $pull: { data: { id: params.subjectId } } })
   })
     
 }
