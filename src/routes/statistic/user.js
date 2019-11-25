f04962827f02debc93477dad98612c08f008b31d
nutsobtid <nuttasak@sobtid.me>
2019-11-25 17:46:18 +0700

    Add user export static

--- src/routes/statistic/user.js
+++ src/routes/statistic/user.js
@@ -140,4 +140,53 @@ module.exports = async (fastify, options) => {
       count: response.count,
     }))
   })
+
+  fastify.get('/user/transactions/:year/:month', {
+    preValidation: [
+      fastify.authenticate(),
+      fastify.authorize([ROLE.ADMIN])
+    ]
+  }, async (request, reply) => {
+    const { params } = request
+
+    const start = moment(`${params.year}${params.month}01000000 `, "YYYYMMDDHHmmss").unix()
+    const end = moment(`${params.year}${params.month}30235959 `, "YYYYMMDDHHmmss").unix()
+
+    const aggregate = [
+      {
+        $match: {
+          createdAt: {
+            $gte: start,
+            $lt: end
+          }
+        }
+      },
+      {
+        $lookup: {
+          from: 'provinces',
+          localField: 'school.province.id',
+          foreignField: '_id',
+          as: 'province'
+        }
+      },
+      {
+        $unwind: "$province"
+      }
+    ] 
+
+    const response = await fastify.mongoose.User.aggregate(aggregate)
+    return response
+    .map((item, index) => ({
+      order: index+1,
+      username: item.username ? item.username : '',
+      email: item.email,
+      firstName: item.firstName,
+      lastName: item.lastName,
+      schoolName: item.school.name.text,
+      provinceName: item.province.name,
+      role: item.role,
+      registeredDate: moment.unix(item.createdAt).add(543, 'y').format('DD/MM/YYYY'),
+      registeredTime: moment.unix(item.createdAt).format('HH:mm:ss'),
+    }))
+  })
 }
