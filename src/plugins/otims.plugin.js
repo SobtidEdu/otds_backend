aafabbc75d873cd41a803ee14a226007fb890aa7
nutsobtid <nuttasak@sobtid.me>
2019-12-11 17:18:16 +0700

    Fixed

--- src/plugins/otims.plugin.js
+++ src/plugins/otims.plugin.js
@@ -2,6 +2,7 @@ const fp = require('fastify-plugin')
 const querystring = require('querystring');
 const axios = require('axios')
 const moment = require('moment')
+const { OTIMS_API_URL, OTIMS_TOKEN, OTIMS_USER } = process.env
 
 const formUrlEncoded = data =>  {
   let items = []
@@ -24,8 +25,55 @@ const formUrlEncoded = data =>  {
   return items.join('&')
 }
 
+const getIndicators = async (params = {}) => {
+  params.RequestedName = OTIMS_USER
+  params.RequestedNo = `${OTIMS_USER}StrandIndicatorRequest${moment().format('YYYYMMDDHHmmSSS')}`
+
+  // let q = querystring.stringify(params)
+  const indicators = await instance.get(`/ws/StrandIndicatorRequest`, { params })
+  .then(response => response.data.Result.StrandList_ResponseStrandIndicator.StrandList.map(strand => ({
+      name: strand.StrandFullname,
+      code: strand.StrandCode,
+      indicators: strand.IndicatorList_StrandList.IndicatorList.map(indicator => ({
+        name: indicator.IndicatorFullname,
+        code: indicator.IndicatorCode
+      }))
+    }))
+  )
+
+  params.RequestType = 1
+  params.TestSetType = 'FI'
+  params.FollowStrand = true
+  params.FollowIndicator = true
+  params.BankType = 'Public'
+  params.ComplexityLevel = '1,2,3'
+
+  for (let strandIndex in indicators) {
+    for (let indicatorIndex in indicators[strandIndex].indicators) {
+      const indicator = indicators[strandIndex].indicators[indicatorIndex]
+      
+      params.Strand = indicators[strandIndex].code
+      params.Indicator = indicators[strandIndex].indicators[indicatorIndex].code
+
+      q = querystring.stringify(params)
+      indicator.noitems = await instance.get(`/ws/RequestItemsInquiry`, { params }).then(response => {
+        const arrayNoQuestionType = response.data.ResponseItemInquiry.ResponseNoQuestionType_ResponseItemInquiry.ResponseNoQuestionType
+        if (!arrayNoQuestionType[0]) return 0
+        const indicators = arrayNoQuestionType[0].Indicator.split(';')
+        return indicators.reduce((noitems, rawIndicator) => {
+          // console.log(rawIndicator)
+          const item = rawIndicator.split(',') // indicatorName, questionType, noQuestions
+          return parseInt(item[2]) + noitems
+        }, 0)
+      })
+      indicators[strandIndex].indicators[indicatorIndex] = indicator
+    }
+  }
+  return indicators
+}
+
 module.exports = fp(async (fastify, options) => {
-  const { OTIMS_API_URL, OTIMS_TOKEN, OTIMS_USER } = fastify.env
+  
 
   const instance = axios.create({
     baseURL: OTIMS_API_URL,
@@ -43,55 +91,10 @@ module.exports = fp(async (fastify, options) => {
       })))
     },
 
-    getIndicators: async (params = {}) => {
-      params.RequestedName = OTIMS_USER
-      params.RequestedNo = `${OTIMS_USER}StrandIndicatorRequest${moment().format('YYYYMMDDHHmmSSS')}`
-
-      // let q = querystring.stringify(params)
-      const indicators = await instance.get(`/ws/StrandIndicatorRequest`, { params })
-      .then(response => response.data.Result.StrandList_ResponseStrandIndicator.StrandList.map(strand => ({
-          name: strand.StrandFullname,
-          code: strand.StrandCode,
-          indicators: strand.IndicatorList_StrandList.IndicatorList.map(indicator => ({
-            name: indicator.IndicatorFullname,
-            code: indicator.IndicatorCode
-          }))
-        }))
-      )
-
-      params.RequestType = 1
-      params.TestSetType = 'FI'
-      params.FollowStrand = true
-      params.FollowIndicator = true
-      params.BankType = 'Public'
-      params.ComplexityLevel = '1,2,3'
-
-      for (let strandIndex in indicators) {
-        for (let indicatorIndex in indicators[strandIndex].indicators) {
-          const indicator = indicators[strandIndex].indicators[indicatorIndex]
-          
-          params.Strand = indicators[strandIndex].code
-          params.Indicator = indicators[strandIndex].indicators[indicatorIndex].code
-
-          q = querystring.stringify(params)
-          indicator.noitems = await instance.get(`/ws/RequestItemsInquiry`, { params }).then(response => {
-            const arrayNoQuestionType = response.data.ResponseItemInquiry.ResponseNoQuestionType_ResponseItemInquiry.ResponseNoQuestionType
-            if (!arrayNoQuestionType[0]) return 0
-            const indicators = arrayNoQuestionType[0].Indicator.split(';')
-            return indicators.reduce((noitems, rawIndicator) => {
-              // console.log(rawIndicator)
-              const item = rawIndicator.split(',') // indicatorName, questionType, noQuestions
-              return parseInt(item[2]) + noitems
-            }, 0)
-          })
-          indicators[strandIndex].indicators[indicatorIndex] = indicator
-        }
-      }
-      return indicators
-    },
+    getIndicators,
 
     getStrands: async (params = {}) => {
-      const strands = await this.getIndicators(params)
+      const strands = await getIndicators(params)
       return strands.map(strand => ({
         name: strand.name,
         code: strand.code,
